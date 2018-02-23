48cd5de41c1f5b15bca07b4354861976c5c179eb
Berry  Semexan <bsemexan@gmail.com>
2018-02-23 15:09:05 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,61 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
+
+  @@all =[]
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist
+    @artist
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|s| s.name == name}
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+end
